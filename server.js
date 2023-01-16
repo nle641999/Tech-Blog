ba52a1992a2331e22531ceefae245c939522da1d
teakoVA <nleb641999@gmail.com>
2023-01-15 17:19:01 -0800

    updated handlebars

--- server.js
+++ server.js
@@ -1,40 +1,45 @@
-const path = require('path');
-require('dotenv').config();
 const express = require('express');
-const routes = require('./controllers/');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const exphbs = require('express-handlebars')
-const session = require('express-session')
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const helpers = require('./utils/helpers');
+const path = require('path');
+const exphbs = require('express-handlebars');
 
-const hbs = exphbs.create({ helpers });
+// helper function
+const helpers = require('./utils/helper');
 
-const sess = {
-    secret: process.env.DB_SESSION_SECRET,
-    cookie: { maxAge: 7200000 },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize
-    })
-  };
+const hbs = exphbs.create({helpers});
+
+// session (connects session to sequelize Database) --> authentication 
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(session(sess));
+app.use(require('./controllers'));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
-  });
+  app.listen(PORT, () => console.log('Now listening'));
+});
